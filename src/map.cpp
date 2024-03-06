a4d9309eafbc92df3b79b21508eae9cf0588d2bf
Adam1000 <azeiser08@outlook.com>
2024-03-06 08:54:29 -0500

    back to the original code

--- src/map.cpp
+++ src/map.cpp
@@ -2,6 +2,7 @@
 #include "include/texturemanager.hpp"
 #include "include/game.hpp"
 #include <fstream>
+#include <sstream>
 #include <iostream>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -17,31 +18,19 @@ Map::~Map()
 void Map::LoadMap(std::string path, int sizeX, int sizeY)
 {
     std::fstream mapFile;
+    char tile;
+    int srcX, srcY;
+
     mapFile.open(path);
 
-    if(!mapFile.is_open())
-    {
-        std::cerr << "Error opening map file: " << path << std::endl;
-        return;
-    }
-
-    int tile;
     for(int y = 0; y < sizeY; y++)
     {
         for(int x = 0; x < sizeX; x++)
         {
-            mapFile >> tile;
-
-            if(!mapFile)
-            {
-                std::cerr << "Error reading from map file: " << path << std::endl;
-                mapFile.close();
-                return;
-            }
-
-            int srcX = (tile % 8) * 32;
-            int srcY = (tile / 8) * 32;
-
+            mapFile.get(tile);
+            srcY = atoi(&tile) * 32;
+            mapFile.get();
+            srcX = atoi(&tile) * 32;
             Game::AddTile(srcX, srcY, x * 64, y * 64);
             mapFile.ignore();
         }
