5e7dc7b601bed0372cc451c1db0bed0426771390
Adam1000 <azeiser08@outlook.com>
2024-06-10 08:58:25 -0400

    removed old assets

--- src/ECS.cpp
+++
@@ -1,7 +0,0 @@
-#include "include/ECS/ECS.hpp"
-
-void Entity::AddGroup(Group mGroup)
-{
-    groupBitset[mGroup] = true;
-    manager.AddToGroup(this, mGroup);
-}
--- src/include/ECS/components.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include "ECS.hpp"
-#include "spritecomponent.hpp"
-#include "transformcomponent.hpp"
-#include "keyboardcontroller.hpp"
-#include "collidercomponent.hpp"
-#include "tilecomponent.hpp"
