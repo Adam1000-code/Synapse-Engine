606726a1afbbbae9a00734d8c2981a8bb7f16ddd
Adam1000 <azeiser08@outlook.com>
2024-06-27 01:54:28 -0400

    added engine header

---
+++ src/core/engine.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "SDL2/SDL.h"
+#include "SDL2/SDL_image.h"
+
+class Engine
+{
+    public:
+        Engine();
+        ~Engine();
+        
+        static Engine* GetInstance()
+        {
+            return s_instance = (s_instance != nullptr) ? s_instance : new Engine();
+        }
+
+        bool Init(const char* title, int width, int height);
+        bool Clean();
+
+        void Update();
+        void Render();
+        void Events();
+        void Quit();
+
+        inline bool isRunning()
+        {
+            return m_isRunning;
+        }
+        inline SDL_Renderer* GetRenderer()
+        {
+            return m_renderer;
+        }
+    
+    private:
+        bool m_isRunning;
+        SDL_Window* m_window;
+        SDL_Renderer* m_renderer;
+        static Engine* s_instance;
+};
