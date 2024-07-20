a3969ac07e8770f9c9cf972c22eb4b3910409e94
Adam1000 <azeiser08@outlook.com>
2024-07-20 11:44:29 -0400

    added input code

---
+++ src/input/input.cpp
@@ -0,0 +1,52 @@
+#include "input.hpp"
+#include <iostream>
+#include "SDL2/SDL.h"
+#include "../core/engine.hpp"
+
+Input* Input::s_instance = nullptr;
+
+Input::Input()
+{
+    m_keyStates = SDL_GetKeyboardState(nullptr);
+}
+
+void Input::InputListen()
+{
+    SDL_Event event;
+
+    while(SDL_PollEvent(&event))
+    {
+        switch(event.type)
+        {
+            case SDL_QUIT:
+                Engine::GetInstance()->Quit();
+                break;
+            case SDL_KEYDOWN:
+                KeyDown();
+                break;
+            case SDL_KEYUP:
+                KeyUp();
+                break;
+        }
+    }
+}
+
+bool Input::GetKeyDown(SDL_Scancode key)
+{
+    if(m_keyStates[key] == 1)
+    {
+        return true;
+    }
+
+    return false;
+}
+
+void Input::KeyUp()
+{
+    m_keyStates = SDL_GetKeyboardState(nullptr);
+}
+
+void Input::KeyDown()
+{
+    m_keyStates = SDL_GetKeyboardState(nullptr);
+}
---
+++ src/input/input.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <iostream>
+#include "SDL2/SDL.h"
+
+class Input
+{
+    public:
+        static Input* GetInstance()
+        {
+            return s_instance = (s_instance != nullptr) ? s_instance : new Input();
+        }
+
+        void InputListen();
+        bool GetKeyDown(SDL_Scancode key);
+    
+    private:
+        Input();
+        void KeyUp();
+        void KeyDown();
+
+        const Uint8* m_keyStates;
+        static Input* s_instance;
+};
