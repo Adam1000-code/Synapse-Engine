31d1a7994979dee827bbacd9d207920d84cd1362
Adam1000 <azeiser08@outlook.com>
2024-09-10 14:09:29 -0400

    changed tilemap and added tilemap assets

---
+++ assets/kings and pigs map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="kings and pigs map" tilewidth="32" tileheight="32" tilecount="247" columns="19">
+ <image source="../Downloads/Kings and Pigs/Sprites/14-TileSets/Terrain (32x32).png" width="608" height="416"/>
+</tileset>
