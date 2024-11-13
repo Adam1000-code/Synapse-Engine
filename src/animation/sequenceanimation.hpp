c937172a986ab4e2225ab4d3f1b45e6a68577784
Adam1000 <azeiser08@outlook.com>
2024-11-13 09:14:06 -0500

    created sequence animation header

---
+++ src/animation/sequenceanimation.hpp
@@ -0,0 +1,36 @@
+#pragma once
+
+#include <map>
+#include <string>
+#include <vector>
+#include <SDL2/SDL.h>
+#include "animation.hpp"
+
+using namespace std;
+
+struct Sequence
+{
+    int speed;
+    int frameCount;
+    int width, height;
+    vector<string> textureIDs;
+};
+
+class SequenceAnimation : public Animation
+{
+    public:
+        SequenceAnimation(bool repeat = true);
+
+        virtual void Update(float deltaTime);
+        void Parse(string source);
+        void SetCurrentSequence(string sequenceID);
+        void SetRepeat(bool m_repeat)
+        {
+            repeat = m_repeat;
+        }
+        void DrawFrame(float x, float y, float scaleX = 1, float scaleY = 1, SDL_RendererFlip flip = SDL_FLIP_NONE);
+
+    private:
+        Sequence CurrentSequence;
+        map<string, Sequence> sequenceMap;
+};
